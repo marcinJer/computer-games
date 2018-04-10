b11d90579012808a7657c72421a26ce1b94a8fea
Marcin <jmarcin511@gmail.com>
2018-04-10 18:53:52 +0200

    Added view with basic info and with details

--- src/test/java/com/example/ComputerGames/ComputerGamesApplicationTests.java
+++ src/test/java/com/example/ComputerGames/ComputerGamesApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.ComputerGames;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import pl.marcin.jer.data.data.ComputerGame;
 import pl.marcin.jer.data.data.ComputerGameValidator;
 import pl.marcin.jer.data.data.TypesOfGames;
+import pl.marcin.jer.repositories.ComputerGameRepository;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
@@ -14,10 +17,42 @@ public class ComputerGamesApplicationTests {
 
 	@Test
 	public void shouldBeInvalidWhenGameNameIsEmpty() {
-		ComputerGame computerGame = new ComputerGame("", TypesOfGames.Action, 12, "Brand", 1);
+		ComputerGame computerGame = new ComputerGame("", TypesOfGames.Action, 18, "CdProject RED", 1);
         ComputerGameValidator computerGameValidator = new ComputerGameValidator();
         boolean result = computerGameValidator.areValuesEmpty(computerGame);
         assertThat(result).isTrue();
+	}
+
+	@Test
+	public void shouldBeInvalidWhenGameTypeIsEmpty() {
+		ComputerGame computerGame = new ComputerGame("Gothic", null, 12, "Piranha Bytes", 1);
+		ComputerGameValidator computerGameValidator = new ComputerGameValidator();
+		boolean result = computerGameValidator.areValuesEmpty(computerGame);
+		assertThat(result).isTrue();
+	}
+
+	@Test
+	public void shouldBeInvalidWhenAllowedAgeIsEmpty() {
+		ComputerGame computerGame = new ComputerGame("Mafia", TypesOfGames.Action, null, "Illusion Softworks", 1);
+		ComputerGameValidator computerGameValidator = new ComputerGameValidator();
+		boolean result = computerGameValidator.areValuesEmpty(computerGame);
+		assertThat(result).isTrue();
+	}
+
+	@Test
+	public void shouldBeInvalidWhenManufacturerIsEmpty() {
+		ComputerGame computerGame = new ComputerGame("Far Cry", TypesOfGames.FPS, 12, "", 1);
+		ComputerGameValidator computerGameValidator = new ComputerGameValidator();
+		boolean result = computerGameValidator.areValuesEmpty(computerGame);
+		assertThat(result).isTrue();
+	}
+
+	@Test
+	public void shouldBeInvalidWhenAllowedAgeLessThanZero() {
+		ComputerGame computerGame = new ComputerGame("Fifa 18", TypesOfGames.Arcade, -12, "EA Sports", 1);
+		ComputerGameValidator computerGameValidator = new ComputerGameValidator();
+		boolean result = computerGameValidator.numericValidate(computerGame);
+		assertThat(result).isFalse();
 	}
 
 }
